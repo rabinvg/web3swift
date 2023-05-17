39e1dd97bcf899f40b152ecd8c5d7b2c16e18fc5
rabin.pun <rabin.pun@ebpearls.com>
2023-05-17 07:02:16 +0545

    revert secp2561.c pod

--- Sources/Web3Core/KeystoreManager/PlainKeystore.swift
+++ Sources/Web3Core/KeystoreManager/PlainKeystore.swift
@@ -4,7 +4,7 @@
 //
 
 import Foundation
-import secp256k1_ios
+import secp256k1
 
 public class PlainKeystore: AbstractKeystore {
 
--- Sources/Web3Core/Structure/SECP256k1.swift
+++ Sources/Web3Core/Structure/SECP256k1.swift
@@ -4,7 +4,7 @@
 //
 
 import Foundation
-import secp256k1_ios
+import secp256k1
 
 public struct SECP256K1 {
     public struct UnmarshaledSignature {
